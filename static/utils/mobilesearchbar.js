fd30cbd6530284f9360ba5acf5e263cab583ff52
Dev Patel <devpatel8907@gmail.com>
2021-08-23 22:05:03 +0530

    august 23 test 2

--- static/utils/mobilesearchbar.js
+++ static/utils/mobilesearchbar.js
@@ -1,26 +1,52 @@
-function expandMobileSearch() {
-    searchDrop.style.height="100vh";
-    setTimeout(()=>{
-        let searchDrop=document.querySelector('.mobile-search-dropdown');
+// function expandMobileSearch() {
+//     searchDrop.style.height="100vh";
+//     setTimeout(()=>{
+//         let searchDrop=document.querySelector('.mobile-search-dropdown');
         
-        searchDrop.style.pointerEvents='auto';
-    },300);
-    document.getElementById('searchbar').value=localStorage.getItem('searchbar');
-    getinput();
-}
+//         searchDrop.style.pointerEvents='auto';
+//     },300);
+//     document.getElementById('searchbar').value=localStorage.getItem('searchbar');
+//     getinput();
+// }
 
-function minimizeMobileSearch(){
-    let searchDrop=document.querySelector('.mobile-search-dropdown');
-    searchDrop.style.height="";
-    setTimeout(()=>{
-        let searchDrop=document.querySelector('.mobile-search-dropdown');
+// function minimizeMobileSearch(){
+//     let searchDrop=document.querySelector('.mobile-search-dropdown');
+//     searchDrop.style.height="";
+//     setTimeout(()=>{
+//         let searchDrop=document.querySelector('.mobile-search-dropdown');
         
-        searchDrop.style.pointerEvents='auto';
-    },300);
-    document.getElementById('searchbar').value=localStorage.getItem('searchbar');
+//         searchDrop.style.pointerEvents='auto';
+//     },300);
+//     document.getElementById('searchbar').value=localStorage.getItem('searchbar');
+// }
+// const searchbar = document.getElementById('searchbar');
+async function getinputmobile() {
+    let value=document.getElementById('mobile-searchbar').value;
+    const response = await fetch('/product/recommandations', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ value })
+    });
+    const result = await response.json();
+    console.log(result);
 }
-const searchbar = document.getElementById('searchbar');
-let searchDropdown = document.querySelector('.searchbar-dropdown');
+const mobilecallingfunction = (fn, d) => {
+    let list;
+    return function () {
+        let content = this, args = arguments;
+        clearTimeout(list);
+        list = setTimeout(() => {
+            fn.apply(content, args);
+        }, d);
+    }
+}
+const mobilesearchresult = mobilecallingfunction(getinputmobile, 300);
+window.onload = () => {
+    getinput();
+};
+// let searchDropdown = document.querySelector('.searchbar-dropdown');
 function executeSearch() {
     window.location.href = "http://localhost:3000/products";
     localStorage.setItem('search',searchbar.value.toLowerCase());
